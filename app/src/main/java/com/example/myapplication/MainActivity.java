393f22daeeff52b1c679999e53d91ff283766591
Saahil-Shelke <saahil.shelke@gmail.com>
2021-11-23 22:34:04 +0530

    Gmaps implement

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.content.Intent;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    public Button button;
     CardView card1;
 
     @Override
@@ -16,6 +18,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         card1 = findViewById(R.id.card1);
+
+        button = (Button) findViewById(R.id.Btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,MapsActivity.class);
+                startActivity(intent);
+
+            }
+        });
 
 
     }
