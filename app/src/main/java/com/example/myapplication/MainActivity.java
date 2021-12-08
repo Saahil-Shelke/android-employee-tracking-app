23579b0f314199c46139d0f2a22987a1beaf5eb9
Saahil-Shelke <saahil.shelke@gmail.com>
2021-12-08 12:34:12 +0530

    navigation drawer

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,21 +1,42 @@
 package com.example.myapplication;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.cardview.widget.CardView;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 import android.widget.Button;
 import android.content.Intent;
 import android.view.View;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class MainActivity extends AppCompatActivity {
     public Button button;
     public Button button1;
+
+    public DrawerLayout drawerLayout;
+    public NavigationView navigationView;
+    public Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navigationView = findViewById(R.id.navigationviews);
+        toolbar = findViewById(R.id.toolbar);
+
+        //setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout,toolbar,R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
 
         button = (Button) findViewById(R.id.Btn);
         button.setOnClickListener(new View.OnClickListener() {
